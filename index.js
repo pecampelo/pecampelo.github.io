53d999d81de5e5acfe60ecf47faa092f02ae767d
pecampelo <pedrohcmatheus@gmail.com>
2020-06-16 23:43:11 -0300

    nice

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let initialButtonPresses = 0
-
-if ()
+function fuckCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+    localStorage.clickcount = Number(local.storage.clickcount) + 1;
+  } else {
+    localStorage.clickcount = 1
+    }
+    document.getElementById ('result').innerHTML = "You have fucked the button "
+     + localStorage.clickcount + " time(s).";     
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
