077ccf872255f1bc0b9065db0fc8428c1376407f
pecampelo <pedrohcmatheus@gmail.com>
2020-08-20 17:40:53 -0300

    Embed NodeJS Server
    using devmedia tutorial

---
+++ server/filehandler.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+module.exports = function(filename, successFn, errorFn) {
+  fs.readFile(filename, function(err, data){
+    if (err) { errorFn(err); }
+    else { successFn(data); }
+  });
+};
