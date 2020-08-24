be1b45a8d06d8e5be1ecdae9e8be0e5569b3f86b
pecampelo <pedrohcmatheus@gmail.com>
2020-08-24 17:42:34 -0300

    nice

---
+++ index.js
@@ -0,0 +1,18 @@
+function openNavOne() {
+  document.getElementById('sidenavOne').style.width = '200px';
+  document.getElementById("main").style.marginLeft = "200px";
+}
+
+function closeNavOne() {
+  document.getElementById('sidenavOne').style.width = '0px';
+  document.getElementById("main").style.marginLeft = "0px";
+}
+function openNavTwo() {
+  document.getElementById('sidenavTwo').style.width = '300px';
+  document.getElementById("main").style.marginRight = "300px";
+}
+
+function closeNavTwo() {
+  document.getElementById('sidenavTwo').style.width = '0px';
+  document.getElementById("main").style.marginRight = "0px";
+}
