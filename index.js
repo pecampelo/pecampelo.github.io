538c73c4821e9e6148c80b0855e025498fc4a75c
Pedro Campelo <pedrohcmatheus@gmail.com>
2020-09-08 13:14:49 -0300

    Revert "Solving issue"
    This reverts commit 7e79d6bd91ef1a1d85dd3f704f6f4d4b09816d67.

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 function openNavOne() {
   document.getElementById('sidenavOne').style.width = '200px';
-<<<<<<< HEAD
   document.getElementById("main").style.marginLeft = "200px";
-=======
-  document.getElementsByName('main').style.marginLeft = "200px";
->>>>>>> parent of 06ee451... project setup and studying methodologies
 }
 
 function closeNavOne() {
   document.getElementById('sidenavOne').style.width = '0px';
-<<<<<<< HEAD
   document.getElementById("main").style.marginLeft = "0px";
 }
 function openNavTwo() {
   document.getElementById('sidenavTwo').style.width = '300px';
   document.getElementById("main").style.marginRight = "300px";
-=======
-  document.getElementsByName('main').style.marginLeft = "0px";
-}
-function openNavTwo() {
-  document.getElementById('sidenavTwo').style.width = '300px';
-  document.getElementsByName('main').style.marginRight = "300px";
->>>>>>> parent of 06ee451... project setup and studying methodologies
 }
 
 function closeNavTwo() {
   document.getElementById('sidenavTwo').style.width = '0px';
-<<<<<<< HEAD
   document.getElementById("main").style.marginRight = "0px";
-=======
-  document.getElementsByTagName('main').style.marginRight = "0px";
->>>>>>> parent of 06ee451... project setup and studying methodologies
 }
